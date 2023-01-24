eb21a9345b766acbf6d20eb79798354702d66a5d
Philips Ucheolisah Nge <philencripted@gmail.com>
2023-01-24 09:59:48 -0500

    update

---
+++ lib/data/furniture_data.dart
@@ -0,0 +1,22 @@
+import 'model/furniture_model.dart';
+
+final furnitureData = [
+  Furniture(
+      id: Furniture.randomId,
+      furnitureName: 'Ox Eye Chair',
+      furniturePrice: '\$240',
+      furnitureRating: '3.5',
+      furnitureDescription:
+          'Fantastic designed Mahagony armed chair, with exotic touch of leather',
+      furnitureImageUrl: 'assets/images/2.png',
+      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+  Furniture(
+      id: Furniture.randomId,
+      furnitureName: 'Bull horn Executive Chair',
+      furniturePrice: '\$2140',
+      furnitureRating: '4.5',
+      furnitureDescription:
+          'Exotic designed Mahagony armed chair, with exotic touch of leather',
+      furnitureImageUrl: 'assets/images/2.png',
+      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+];
--- lib/data/model/furniture_model.dart
+++ lib/data/model/furniture_model.dart
@@ -1,14 +1,22 @@
 class Furniture {
+  final String id;
   final String furnitureName;
   final String furniturePrice;
   final String furnitureRating;
   final String furnitureDescription;
   final String furnitureImageUrl;
+  final List<String> furnitureThumbNailsUrl;
 
   Furniture(
-      {required this.furnitureName,
+      {required this.id,
+      required this.furnitureName,
       required this.furniturePrice,
       required this.furnitureRating,
       required this.furnitureDescription,
-      required this.furnitureImageUrl});
+      required this.furnitureImageUrl,
+      required this.furnitureThumbNailsUrl});
+
+  static get randomId {
+    return DateTime.now().millisecondsSinceEpoch.toString();
+  }
 }
