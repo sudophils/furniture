ce2408e4321795873e5887385b519f6ecc20dcb2
Philips Ucheolisah Nge <philencripted@gmail.com>
2023-01-26 11:50:38 -0500

    done

--- lib/data/furniture_data.dart
+++ lib/data/furniture_data.dart
@@ -9,32 +9,32 @@ final furnitureData = [
       furnitureDescription:
           'Fantastic designed Mahagony armed chair, with exotic touch of leather',
       furnitureImageUrl: 'assets/images/2.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureThumbNailsUrl: ['assets/images/7.png', 'assets/images/3.png']),
   Furniture(
       id: Furniture.randomId,
-      furnitureName: 'Ox Eye Chair',
+      furnitureName: 'Fantasy Rocker Chair',
       furniturePrice: '240',
-      furnitureRating: '3.5',
+      furnitureRating: '3.1',
       furnitureDescription:
           'Fantastic designed Mahagony armed chair, with exotic touch of leather',
       furnitureImageUrl: 'assets/images/1.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureThumbNailsUrl: ['assets/images/4.png', 'assets/images/6.png']),
   Furniture(
       id: Furniture.randomId,
-      furnitureName: 'Bull horn Executive Chair',
+      furnitureName: 'Bull Onyx Green  Chair',
       furniturePrice: '2140',
       furnitureRating: '4.5',
       furnitureDescription:
           'Exotic designed Mahagony armed chair, with exotic touch of leather',
       furnitureImageUrl: 'assets/images/5.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureThumbNailsUrl: ['assets/images/3.png', 'assets/images/1.png']),
   Furniture(
       id: Furniture.randomId,
-      furnitureName: 'Bull horn Executive Chair',
+      furnitureName: 'Executive Whitehead Chair',
       furniturePrice: '2140',
-      furnitureRating: '4.5',
+      furnitureRating: '2.5',
       furnitureDescription:
           'Exotic designed Mahagony armed chair, with exotic touch of leather',
       furnitureImageUrl: 'assets/images/6.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureThumbNailsUrl: ['assets/images/7.png', 'assets/images/1.png']),
 ];
