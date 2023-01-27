c9632029668beaef4c95f17aa6b38c6b45f641b5
Philips Ucheolisah Nge <philencripted@gmail.com>
2023-01-27 02:32:48 -0500

    update data

--- lib/data/furniture_data.dart
+++ lib/data/furniture_data.dart
@@ -4,19 +4,37 @@ final furnitureData = [
   Furniture(
       id: Furniture.randomId,
       furnitureName: 'Ox Eye Chair',
-      furniturePrice: '\$240',
+      furniturePrice: '240',
       furnitureRating: '3.5',
       furnitureDescription:
           'Fantastic designed Mahagony armed chair, with exotic touch of leather',
       furnitureImageUrl: 'assets/images/2.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureThumbNailsUrl: ['assets/images/7.png', 'assets/images/3.png']),
   Furniture(
       id: Furniture.randomId,
-      furnitureName: 'Bull horn Executive Chair',
-      furniturePrice: '\$2140',
+      furnitureName: 'Fantasy Rocker Chair',
+      furniturePrice: '240',
+      furnitureRating: '3.1',
+      furnitureDescription:
+          'Fantastic designed Mahagony armed chair, with exotic touch of leather',
+      furnitureImageUrl: 'assets/images/1.png',
+      furnitureThumbNailsUrl: ['assets/images/4.png', 'assets/images/6.png']),
+  Furniture(
+      id: Furniture.randomId,
+      furnitureName: 'Bull Onyx Green  Chair',
+      furniturePrice: '2140',
       furnitureRating: '4.5',
       furnitureDescription:
           'Exotic designed Mahagony armed chair, with exotic touch of leather',
-      furnitureImageUrl: 'assets/images/2.png',
-      furnitureThumbNailsUrl: ['assets/images/2.png', 'assets/images/3.png']),
+      furnitureImageUrl: 'assets/images/5.png',
+      furnitureThumbNailsUrl: ['assets/images/3.png', 'assets/images/1.png']),
+  Furniture(
+      id: Furniture.randomId,
+      furnitureName: 'Executive Whitehead Chair',
+      furniturePrice: '2140',
+      furnitureRating: '2.5',
+      furnitureDescription:
+          'Exotic designed Mahagony armed chair, with exotic touch of leather',
+      furnitureImageUrl: 'assets/images/6.png',
+      furnitureThumbNailsUrl: ['assets/images/7.png', 'assets/images/1.png']),
 ];
